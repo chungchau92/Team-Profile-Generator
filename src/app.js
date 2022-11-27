623fbecbdda978577d8d71b95bfe315f0baf6b3a
chungchau92 <chungchau92@gmail.com>
2022-11-27 23:25:09 +0700

    update file lib

---
+++ src/app.js
@@ -0,0 +1,96 @@
+const inquire = require("inquire")
+
+const managerQuestion = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "enter manager's name"
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "enter manager's id"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "enter manager's email"
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: "enter manager's office number"
+    },
+    {
+        type: 'list',
+        name: 'option',
+        message: 'add an engineer or an intern or to finish building a team',
+        choice : [' add an engineer','add an intern','finish']
+    }
+]
+
+const engineerQuestion = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "enter engineer's name"
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "enter engineer's id"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "enter engineer's email"
+    },
+    {
+        type: 'input',
+        name: 'gitHub',
+        message: "enter GitHub username"
+    },
+    {
+        type: 'list',
+        name: 'option',
+        message: 'add an engineer or an intern or to finish building a team',
+        choice : [' add an engineer','add an intern','finish']
+    }
+]
+
+const internQuestion = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "enter intern's name"
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "enter intern's id"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "enter intern's email"
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: "enter intern's school"
+    },
+    {
+        type: 'list',
+        name: 'option',
+        message: 'add an engineer or an intern or to finish building a team',
+        choice : [' add an engineer','add an intern','finish']
+    }
+]
+
+class app {
+    constructor() {
+        this.List = []
+    }
+
+    
+}
