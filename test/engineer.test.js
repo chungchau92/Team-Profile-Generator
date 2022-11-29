d70be8c96eb1ef2db84c09a567747a698aaf6f91
chungchau92 <chungchau92@gmail.com>
2022-11-29 23:39:47 +0700

    fix packag.json test's value change to jest

--- test/engineer.test.js
+++ test/engineer.test.js
@@ -1,33 +1,33 @@
-// const Engineer = require("../lib/engineer")
+const Engineer = require("../lib/engineer")
 
-// describe("Engineer Class", () => {
-//     it("compare Engineer name", () => {
-//         const engineer = new Engineer("aaaaaaaa",1,"aaaaaaa.com","aaaaaaa")
-//         const result = engineer.name
-//         expect(result).toEqual("aaaaaaaa")
-//     })
+describe("Engineer Class", () => {
+    it("compare Engineer name", () => {
+        const engineer = new Engineer("aaaaaaaa",1,"aaaaaaa.com","aaaaaaa")
+        const result = engineer.name
+        expect(result).toEqual("aaaaaaaa")
+    })
 
-//     it("compare Engineer id", () => {
-//         const engineer = new Engineer("aaaaaaaa",1,"aaaaaaa.com","aaaaaaa")
-//         const result = engineer.id
-//         expect(result).toEqual(1)
-//     })
+    it("compare Engineer id", () => {
+        const engineer = new Engineer("aaaaaaaa",1,"aaaaaaa.com","aaaaaaa")
+        const result = engineer.id
+        expect(result).toEqual(1)
+    })
 
-//     it("compare Engineer email", () => {
-//         const engineer = new Engineer("aaaaaaaa",1,"aaaaaaa.com","aaaaaaa")
-//         const result = engineer.email
-//         expect(result).toEqual("aaaaaaa.com")
-//     })
+    it("compare Engineer email", () => {
+        const engineer = new Engineer("aaaaaaaa",1,"aaaaaaa.com","aaaaaaa")
+        const result = engineer.email
+        expect(result).toEqual("aaaaaaa.com")
+    })
 
-//     it("compare Engineer role", () => {
-//         const engineer = new Engineer("aaaaaaaa",1,"aaaaaaa.com","aaaaaaa")
-//         const result = engineer.getRole()
-//         expect(result).toEqual("Engineer")
-//     })
+    it("compare Engineer role", () => {
+        const engineer = new Engineer("aaaaaaaa",1,"aaaaaaa.com","aaaaaaa")
+        const result = engineer.getRole()
+        expect(result).toEqual("Engineer")
+    })
 
-//     it("compare Engineer Github username", () => {
-//         const engineer = new Engineer("aaaaaaaa",1,"aaaaaaa.com","aaaaaaa")
-//         const result = engineer.github
-//         expect(result).toEqual("aaaaaaa")
-//     })
-// })
+    it("compare Engineer Github username", () => {
+        const engineer = new Engineer("aaaaaaaa",1,"aaaaaaa.com","aaaaaaa")
+        const result = engineer.github
+        expect(result).toEqual("aaaaaaa")
+    })
+})
--- test/manager.test.js
+++ test/manager.test.js
@@ -1,33 +1,33 @@
-// const Manager = require("../lib/manager")
+const Manager = require("../lib/manager")
 
-// describe("Manager Class", () => {
-//     it("compare manager name", () => {
-//         const manager = new Manager("aaaaaaaa",1,"aaaaaaa.com",1)
-//         const result = manager.name
-//         expect(result).toEqual("aaaaaaaa")
-//     })
+describe("Manager Class", () => {
+    it("compare manager name", () => {
+        const manager = new Manager("aaaaaaaa",1,"aaaaaaa.com",1)
+        const result = manager.name
+        expect(result).toEqual("aaaaaaaa")
+    })
 
-//     it("compare manager id", () => {
-//         const manager = new Manager("aaaaaaaa",1,"aaaaaaa.com",1)
-//         const result = manager.id
-//         expect(result).toEqual(1)
-//     })
+    it("compare manager id", () => {
+        const manager = new Manager("aaaaaaaa",1,"aaaaaaa.com",1)
+        const result = manager.id
+        expect(result).toEqual(1)
+    })
 
-//     it("compare manager email", () => {
-//         const manager = new Manager("aaaaaaaa",1,"aaaaaaa.com",1)
-//         const result = manager.email
-//         expect(result).toEqual("aaaaaaa.com")
-//     })
+    it("compare manager email", () => {
+        const manager = new Manager("aaaaaaaa",1,"aaaaaaa.com",1)
+        const result = manager.email
+        expect(result).toEqual("aaaaaaa.com")
+    })
 
-//     it("compare manager role", () => {
-//         const manager = new Manager("aaaaaaaa",1,"aaaaaaa.com",1)
-//         const result = manager.getRole()
-//         expect(result).toEqual("manager")
-//     })
+    it("compare manager role", () => {
+        const manager = new Manager("aaaaaaaa",1,"aaaaaaa.com",1)
+        const result = manager.getRole()
+        expect(result).toEqual("Manager")
+    })
 
-//     it("compare manager officeNumber", () => {
-//         const manager = new Manager("aaaaaaaa",1,"aaaaaaa.com",1)
-//         const result = manager.getRole()
-//         expect(result).toEqual(1)
-//     })
-// })
+    it("compare manager officeNumber", () => {
+        const manager = new Manager("aaaaaaaa",1,"aaaaaaa.com",1)
+        const result = manager.officeNumber
+        expect(result).toEqual(1)
+    })
+})
